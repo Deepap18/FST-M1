33d0db05ae2cebaf415e3ade15e1c0584e2d6166
deepapatil <deepapatil@in.ibm.com>
2022-05-02 14:54:17 +0530

    adding python activities files

---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,9 @@
+Num = int(input("enter the number : "))
+mod = Num % 2
+
+if mod > 0:
+    print("You picked an odd number.")
+else:
+    print("You picked an even number.")
+
+
---
+++ Python/Activities/read_csv.py
@@ -0,0 +1,3 @@
+import pandas
+dataframe = pandas.read_csv('resources/employees.csv')
+print(dataframe)
